94c568ca886d7ab320d4a746474cb981ea538441
rndMemex <csi13@outlook.com>
2020-04-25 09:52:35 +0100

    Fixed Point Representation

---
+++ CHAPTER1/NumbersAndPrecision/1.1 Fixed-Point Representation/fixedpointRep2baseX.go
@@ -0,0 +1,7 @@
+//fixedpointRep2baseX.go - base-10 to base-2 calculator
+//Usage: go build fixedpointRep2baseX.go; ./fixedpointRep2baseX
+//Copyright (c) 2020 rndMemex
+
+package main
+
+func main() {}
