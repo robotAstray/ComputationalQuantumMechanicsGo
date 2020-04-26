0eb2efca9045e2ac80edda92ab435a7540cda863
rndMemex <csi13@outlook.com>
2020-04-26 23:41:12 +0100

    floating point represenatin

---
+++ CHAPTER1/NumbersAndPrecision/1.2 Floating-Point Representation/floatingpointRepresentation.go
@@ -0,0 +1,12 @@
+//floatingpointRep.go - base-10 to base-2 calculator to floating point representation
+//Usage: go build floatingpointRep.go; ./floatingpointRep
+//Copyright (c) 2020 rndMemex
+
+package main
+
+import ()
+
+
+func main(){
+
+}
