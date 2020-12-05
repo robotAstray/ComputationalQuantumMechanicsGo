176316d12d671a70f8dd4710f60700b5bab2df25
rndmemex <rndmemex@protonmail.com>
2020-12-05 16:28:25 +0000

    printing all squares less than 60

---
+++ CHAPTER3/3.6 Loops and Flow Control/3.6.1 The for loop/squares.go
@@ -0,0 +1,27 @@
+//squares.go
+//Printing out all squares numbers less than 60
+
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
+func main() {
+	for i := 0; i < 60; i++ {
+		square := i * i
+		if square > 60 {
+			goodbye()
+		}
+		log.Println(square)
+	}
+
+}
+
+func goodbye() {
+	fmt.Printf("\nGoodbye!\n")
+	fmt.Printf("Copyright(c) 2020 rndmemex@cantab.net\n")
+	os.Exit(0)
+}
